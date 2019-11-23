cc2d6e9d867215dec7e96d9ba7a9d1dab1d97f62
Dante Calderon <mail@dantecalderon.dev>
2019-11-23 12:11:12 -0500

    Parse code

---
+++ src/components/Split.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import Editor from './Editor'
+import { parseJSON2JS } from '../utils/parsers'
+
+const Split = () => {
+  const [json, setJson] = useState('')
+  const [js, setJS] = useState('')
+
+  useEffect(() => {
+    const res = parseJSON2JS(json)
+    if (res) {
+      console.log('ajaj')
+      setJS(res)
+    }
+  }, [json])
+
+  console.log('Dante: Split -> json', json)
+  console.log('Dante: Split -> js', js)
+
+  return (
+    <Container>
+      <Left>
+        <Editor
+          tabSize={2}
+          onChange={value => setJson(value)}
+          value={json}
+          name="left"
+          placeholder="Put your JSON here"
+          theme="github"
+          fontSize={15}
+          mode="json"
+        />
+      </Left>
+      <Right>
+        <Editor
+          name="right"
+          tabSize={2}
+          value={js}
+          theme="github"
+          fontSize={15}
+          mode="javascript"
+        />
+      </Right>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  display: flex;
+`
+
+const Left = styled.div`
+  border: 1px solid red;
+`
+const Right = styled.div`
+  border: 1px solid blue;
+`
+export default Split
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 import PropTypes from 'prop-types'
-import { useStaticQuery, graphql } from 'gatsby'
-
+import React from 'react'
 import Header from './header'
-import './layout.css'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -18,7 +16,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      rs
       <div
         style={{
           margin: `0 auto`,
